9eca3b61cd4c3516792a2149e5f459125835ea53
Kate <kate.vinnik393@gmail.com>
2020-03-16 16:47:37 +0300

    Added transfer message between different ports

--- chat-sample/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ chat-sample/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -1,9 +1,12 @@
 package com.example.messagingstompwebsocket;
 
+import com.example.messagingstompwebsocket.config.EmbeddedTomcatConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ EmbeddedTomcatConfiguration.class})
 public class MessagingStompWebsocketApplication {
 
     public static void main(String[] args) {
---
+++ chat-sample/src/main/java/com/example/messagingstompwebsocket/config/EmbeddedTomcatConfiguration.java
@@ -0,0 +1,57 @@
+package com.example.messagingstompwebsocket.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.google.common.collect.Sets;
+
+@Configuration
+public class EmbeddedTomcatConfiguration {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${management.port:${server.port}}")
+    private String managementPort;
+
+    @Value("${server.additionalPorts:null}")
+    private String additionalPorts;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector[] additionalConnectors = this.additionalConnector();
+        if (additionalConnectors != null && additionalConnectors.length > 0) {
+            tomcat.addAdditionalTomcatConnectors(additionalConnectors);
+        }
+        return tomcat;
+    }
+
+    private Connector[] additionalConnector() {
+        if (StringUtils.isBlank(this.additionalPorts)) {
+            return null;
+        }
+        Set<String> defaultPorts = Sets.newHashSet(this.serverPort, this.managementPort);
+        String[] ports = this.additionalPorts.split(",");
+        List<Connector> result = new ArrayList<>();
+        for (String port : ports) {
+            if (!defaultPorts.contains(port)) {
+                Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+                connector.setScheme("http");
+                connector.setPort(Integer.valueOf(port));
+                result.add(connector);
+            }
+        }
+        return result.toArray(new Connector[] {});
+    }
+}
+
+
