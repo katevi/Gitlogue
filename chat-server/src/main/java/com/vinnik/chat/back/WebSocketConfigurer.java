f83f283162f09ac703e72ec7cdde6059809b7ce8
Kate <kate.vinnik393@gmail.com>
2020-03-21 10:04:30 +0300

    Added server draft

--- chat-server/src/main/java/com/vinnik/chat/back/WebSocketConfigurer.java
+++ chat-server/src/main/java/com/vinnik/chat/back/WebSocketConfigurer.java
@@ -12,13 +12,13 @@ public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        //config.enableSimpleBroker("/topic");
-        //config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
     }
 
 }
