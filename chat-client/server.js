553f5e8c6458bc46d2adbb7620113ed9ef1e2e66
Kate <kate.vinnik393@gmail.com>
2020-04-05 18:22:15 +0300

    Started add post requests from html form

--- chat-client/server.js
+++ chat-client/server.js
@@ -1,10 +1,48 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 8081
 
-app.get('/', (request, response) => {
-    response.send('Hello from Express!')
-})
+const stompjs = require('stompjs');
+const sockjs = require('sockjs-client');
+
+var stompClient = null;
+
+app.post('/connection', (request, response) => {
+	response.sendFile(__dirname + `/index.html`);
+	connect();
+	response.redirect('/');
+});
+
+app.post('/sendedMessages', (request, response) => {
+	console.log('Sended');
+});
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#dialogue").html("");
+}
+
+function connect() {
+    var stompClient = new StompJs.Client('http://localhost:8080/chat-websocket/');
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/publishedMessages', function (dialogue) {
+            showMessage(JSON.parse(dialogue.body).content);
+        });
+    });
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + `/index.html`);
+});
 
 app.listen(port, (err) => {
     if (err) {
