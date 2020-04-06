1770c54a9df103e8fbc3729f9409724f2a221356
Kate <kate.vinnik393@gmail.com>
2020-04-06 15:31:38 +0300

    Added simple server for serving html page and js module

--- chat-client/server.js
+++ chat-client/server.js
@@ -2,47 +2,14 @@ const express = require('express')
 const app = express()
 const port = 8081
 
-const stompjs = require('stompjs');
-const sockjs = require('sockjs-client');
 
-var stompClient = null;
-
-app.post('/connection', (request, response) => {
-	response.sendFile(__dirname + `/index.html`);
-	connect();
-	response.redirect('/');
-});
-
-app.post('/sendedMessages', (request, response) => {
-	console.log('Sended');
-});
-
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#dialogue").html("");
-}
-
-function connect() {
-    var stompClient = new StompJs.Client('http://localhost:8080/chat-websocket/');
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/publishedMessages', function (dialogue) {
-            showMessage(JSON.parse(dialogue.body).content);
-        });
-    });
-}
+app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + `/index.html`);
+    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/app.js');
 });
+
 
 app.listen(port, (err) => {
     if (err) {
