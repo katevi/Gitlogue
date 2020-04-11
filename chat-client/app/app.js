2dd09013d7fdee9d503369f5d75f1bf685bb3b87
Kate <kate.vinnik393@gmail.com>
2020-04-11 21:52:47 +0300

    Renamed error (wrote variable incorrectly)

--- chat-client/app/app.js
+++ chat-client/app/app.js
@@ -14,7 +14,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS(websocketAddress);
+    var socket = new SockJS(serverAddress);
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
