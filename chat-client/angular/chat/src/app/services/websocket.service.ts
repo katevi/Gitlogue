a711bcff795e15460ed62f48a172790a73ee534b
Andrey Koltsov <a99920641@gmail.com>
2020-04-12 21:12:10 +0300

    Format code.

--- chat-client/angular/chat/src/app/services/websocket.service.ts
+++ chat-client/angular/chat/src/app/services/websocket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import * as Stomp from 'stompjs'; 
+import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client'
 
 @Injectable({
@@ -10,22 +10,21 @@ export class WebsocketService {
 
   private TARGET_MSG_SERVER = 'http://localhost:8080';
   private MSG_SERVER_SOCKET_URL = `${this.TARGET_MSG_SERVER}/chat-websocket`;
-  
+
   private stompClient;
 
-  public getMsgPublishSubscription() { 
+  public getMsgPublishSubscription() {
     return this.stompClient;
   }
-  public establishConnection() { 
+  public establishConnection() {
     let socket = new SockJS(this.MSG_SERVER_SOCKET_URL);
     this.stompClient = Stomp.over(socket);
 
-
-    var connectCallback = function() {
+    var connectCallback = function () {
       console.log("connected");
     };
     this.stompClient.connect({}, connectCallback);
-    var receiveMsgCallback = function(message) {
+    var receiveMsgCallback = function (message) {
       console.log(JSON.parse(message.body).content);
     }
 
@@ -33,6 +32,6 @@ export class WebsocketService {
   }
 
   public sendMsg(msg: String) {
-    this.stompClient.send("/app/sendedMessages", {}, JSON.stringify({'name': msg}));
+    this.stompClient.send("/app/sendedMessages", {}, JSON.stringify({ 'name': msg }));
   }
 } 
