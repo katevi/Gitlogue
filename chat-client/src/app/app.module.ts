cdd5b693d5e9eb5cca8b4a7e406e171515c11d67
Kate <kate.vinnik393@gmail.com>
2020-04-26 17:01:00 +0300

    Adds working skeleton for navigating between login and register forms

--- chat-client/src/app/app.module.ts
+++ chat-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +13,7 @@ import { appRoutingModule } from './app-routing.module';
 @NgModule({
     imports: [
         BrowserModule, 
+        ReactiveFormsModule,
         FormsModule,
         HttpClientModule,
         appRoutingModule
