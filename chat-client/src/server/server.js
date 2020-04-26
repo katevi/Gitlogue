fe1cf2d1caefdcd83a3ce2820eb3be8284c21fb4
Andrey Koltsov <a99920641@gmail.com>
2020-04-26 20:57:52 +0300

    Save uncommited changes.

---
+++ chat-client/src/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 8081
+
+app.use(express.static('app'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/app/index.html');
+    res.sendFile(__dirname + '/app/app.js');
+});
+
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.err('Unable to start server. Details ${err}')
+    }
+    console.log(`server is listening on ${port}`)
+})
